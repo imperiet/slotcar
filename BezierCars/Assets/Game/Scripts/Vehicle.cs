5384ea410e2e11b2989f3627a1c34ef540ea91ec
thor_sone <thor.sone@vobling.com>
2022-09-30 08:15:42 +0200

    Takeoff limit is now calculated based on car speed and car rotation

--- BezierCars/Assets/Game/Scripts/Vehicle.cs
+++ BezierCars/Assets/Game/Scripts/Vehicle.cs
@@ -6,15 +6,21 @@ using UnityEngine;
 
 public class Vehicle : MonoBehaviour
 {
-    [Foldout("Object References")] [SerializeField] VehicleInput input;
+    [Foldout("Object References")][SerializeField] VehicleInput input;
 
-    [Foldout("Object References")] [SerializeField] private Rigidbody carRigidbody, slotPinRigidbody;
-    [Foldout("Object References")] [SerializeField] private SplineMesh.Spline spline;
-    [SerializeField] AnimationCurve breakingForceCurve;
+    [Foldout("Object References")][SerializeField] private Rigidbody carRigidbody, slotPinRigidbody;
+    [Foldout("Object References")][SerializeField] private SplineMesh.Spline spline;
+    [SerializeField] AnimationCurve maxCarRotationBasedOnSpeed;
     [SerializeField] float drivingForceModifier, slowestSpeed, breakingAngle, breakingSpeed;
-    [ShowNonSerializedField] float currentDrivingForce, relativeAngle;
+    [ShowNonSerializedField] float currentDrivingForce, relativeAngle, lastRelativeAngle, carRotationSpeed, maxCarRotationSpeedRelativeTrack;
+    [ShowNonSerializedField] bool disableBreakoff,outsideOfMaxAngle;
 
-    public float RelativeAngle { get => relativeAngle; }
+
+    public float CurrentDrivingForce { get => currentDrivingForce; set => currentDrivingForce = value; }
+    public float RelativeAngle { get => relativeAngle; set => relativeAngle = value; }
+    public float LastRelativeAngle { get => lastRelativeAngle; set => lastRelativeAngle = value; }
+    public float CarRotationSpeed { get => carRotationSpeed; set => carRotationSpeed = value; }
+    public float MaxCarRotationSpeedRelativeTrack { get => maxCarRotationSpeedRelativeTrack; set => maxCarRotationSpeedRelativeTrack = value; }
 
     private void OnEnable()
     {
@@ -48,17 +54,17 @@ public class Vehicle : MonoBehaviour
 
     public void Drive(float throttle)
     {
-        currentDrivingForce = throttle;
+        CurrentDrivingForce = throttle;
     }
     private void StopThrottle()
     {
-        currentDrivingForce = 0;
+        CurrentDrivingForce = 0;
     }
     void FixedUpdate()
     {
-        if (currentDrivingForce != 0)
+        if (CurrentDrivingForce != 0)
         {
-            carRigidbody.AddForce(carRigidbody.transform.TransformDirection(Vector3.forward) * currentDrivingForce * drivingForceModifier);
+            carRigidbody.AddForce(carRigidbody.transform.TransformDirection(Vector3.forward) * CurrentDrivingForce * drivingForceModifier);
         }
         else if (carRigidbody.velocity.magnitude < slowestSpeed)
         {
@@ -70,22 +76,40 @@ public class Vehicle : MonoBehaviour
         slotPinRigidbody.transform.position = spline.GetProjectionSample(slotPinRigidbody.position).location;
         slotPinRigidbody.transform.rotation = spline.GetProjectionSample(slotPinRigidbody.position).Rotation;
 
+
+
+
         //Calculate angle relative track
         Vector3 trackForwardVector = spline.GetProjectionSample(slotPinRigidbody.position).Rotation * Vector3.back;
-        relativeAngle = Vector3.Angle(carRigidbody.transform.forward, trackForwardVector);
-        relativeAngle = relativeAngle >= 90f ? 180f - relativeAngle : relativeAngle;
-
-        //If Both max angle and a max speed is exceeded, Break car off track.
-        if (relativeAngle > breakingAngle && carRigidbody.velocity.magnitude > breakingSpeed)
-        {
-            Debug.Log("Break");
-            StartCoroutine(Break());
-        }
-
+        RelativeAngle = Vector3.Angle(carRigidbody.transform.forward, trackForwardVector);
+        RelativeAngle = RelativeAngle >= 90f ? 180f - RelativeAngle : RelativeAngle;
 
         //Debug
         Debug.DrawRay(slotPinRigidbody.transform.position, trackForwardVector);
         Debug.DrawRay(slotPinRigidbody.transform.position, carRigidbody.transform.forward);
+
+        //Calculate "flick" force
+        CarRotationSpeed = Mathf.Abs(LastRelativeAngle - RelativeAngle);
+        maxCarRotationSpeedRelativeTrack = maxCarRotationBasedOnSpeed.Evaluate(carRigidbody.velocity.magnitude);
+        LastRelativeAngle = RelativeAngle;
+
+        outsideOfMaxAngle = false;
+        //If Both max angle and a max speed is exceeded, Break car off track.
+        if (RelativeAngle > breakingAngle)
+        {
+            if (CarRotationSpeed > maxCarRotationSpeedRelativeTrack)
+            {
+                outsideOfMaxAngle = true;
+
+                Debug.Log("Break");
+                if (!disableBreakoff)
+                {
+                    StartCoroutine(Break());
+                }
+            }
+        }
+
+
 
     }
 
@@ -95,5 +119,8 @@ public class Vehicle : MonoBehaviour
         carRigidbody.velocity = Vector3.zero;
         yield return new WaitForSeconds(1f);
         carRigidbody.isKinematic = false;
+        disableBreakoff = true;
+        yield return new WaitForSeconds(.5f);
+        disableBreakoff = false;
     }
 }
--- BezierCars/Assets/Game/Scripts/VehicleDebugGraphics.cs
+++ BezierCars/Assets/Game/Scripts/VehicleDebugGraphics.cs
@@ -6,12 +6,18 @@ using UnityEngine.UI;
 public class VehicleDebugGraphics : MonoBehaviour
 {
     [SerializeField] Vehicle vehicle;
-    [SerializeField] Slider slider;
+    [SerializeField] Slider drivingForceSlider,
+        relativeAngleSlider,
+        carRotationSpeedSlider,
+        maxCarRotationSpeedRelativeTrackSlider;
     void Start(){
         if(!vehicle) vehicle = FindObjectOfType<Vehicle>();
     }
     void Update (){
-        slider.value = vehicle.RelativeAngle/90f;
-        slider.targetGraphic.color = vehicle.RelativeAngle>30 ? Color.red : Color.white;
+        drivingForceSlider.value = vehicle.CurrentDrivingForce;
+        carRotationSpeedSlider.value = vehicle.CarRotationSpeed;
+        maxCarRotationSpeedRelativeTrackSlider.value = vehicle.MaxCarRotationSpeedRelativeTrack;
+        relativeAngleSlider.value = vehicle.RelativeAngle/90f;
+        relativeAngleSlider.targetGraphic.color = vehicle.RelativeAngle>30 ? Color.red : Color.white;
     }
 }
